d303752c76346f46494a1a3b67a16cf92ff4e122
delossantos25 <milesdelossantos25@gmail.com>
2025-08-27 15:12:40 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Miles"
+password = "pogiako123"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if uname == username and pword == password:
+    print("ACCESS GRANTED")
+else:   
+    print("ACCESS DENIED")
