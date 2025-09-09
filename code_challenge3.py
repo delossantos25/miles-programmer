427dd957a1033c47ea72be8e044f94bc5f5f7eb5
delossantos25 <milesdelossantos25@gmail.com>
2025-09-09 15:05:15 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,14 @@
+name = input("Whats your name ---> ")
+print("from Lucena to Sariaya is 20 pero merong student discount".upper())
+isStudent = input("Are you a student? (yes or no) ---> ")
+
+if isStudent.lower() == "yes":
+    print("Congrats your eligible for student discount", name)
+    fare = 20 * .2
+    new_fare = 20 - fare
+    print("Dahil ikaw ay student ang babayaran mo nalang ay", new_fare)
+    bayad = eval(input("Enter your bayad ---> "))
+    sukli = bayad - new_fare 
+    print("Your change is", sukli)
+else:
+    print("Sorry, your not eligible for student discount", name, ":<")
