c8598ecd4ac4798948ba614800fb5686f026024e
delossantos25 <milesdelossantos25@gmail.com>
2025-09-15 20:31:11 +0800

    Add files via upload

---
+++ code_challenge5.py
@@ -0,0 +1,14 @@
+# codechallenges.py
+
+print("The FACTORIAL program")
+
+# ask user for input
+num = int(input("Enter any number: "))
+
+# compute factorial
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+
+# show result
+print(f"The Factorial of {num} is {factorial}")
