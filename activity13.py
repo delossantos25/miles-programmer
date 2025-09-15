3c9ee48150769784233343d47b33da22587ded5b
delossantos25 <milesdelossantos25@gmail.com>
2025-09-15 20:40:35 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,36 @@
+#ask user to input ten(10) numbers
+
+#after get the summation of all numbers
+
+sum = 0
+for y in range(1, 11, 1):
+    number = eval(input("Enter any number ---> "))
+
+print("The sum of all the numbers given is ",sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
