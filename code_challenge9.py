8c79ebd868ffd574401b2932b0ae6cddcfbac802
delossantos25 <milesdelossantos25@gmail.com>
2025-09-17 20:44:32 +0800

    Update code_challenge9.py

--- code_challenge9.py
+++ code_challenge9.py
@@ -1,5 +1,6 @@
 print("🦜 PARROT SIMULATOR – THE ECHO CHAMBER!")
-ask = str(input("What do you want your parrot to say? "))
-times = eval(input("How many times should the parrot squawk it? "))
-for x in range(times,0,-1):
-    print("🦜 Squawk!",ask)      
+miles = str(input("What do you want your parrot to say? "))
+pogi = eval(input("How many times should the parrot squawk it? "))
+for m in range(pogi,0,-1):
+
+    print("🦜 Squawk!",miles)      
