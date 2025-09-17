4c927102312954fd40d7bb6f0d07511367177f30
delossantos25 <milesdelossantos25@gmail.com>
2025-09-17 20:40:02 +0800

    Update code_challenge8.py

--- code_challenge8.py
+++ code_challenge8.py
@@ -1,5 +1,6 @@
 num = eval(input("Input a number: "))
 print("Multiplication table for",num,":")
 
-for y in range(1,11,1):
-    print(num,"x",y,"=",num * y)
+for m in range(1,11,1):
+
+    print(num,"x",m,"=",num * m)
