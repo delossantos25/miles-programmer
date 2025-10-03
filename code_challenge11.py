a36c233e1bd67750ae8f080222cb50cc2bcc1c39
delossantos25 <milesdelossantos25@gmail.com>
2025-10-03 11:56:27 +0800

    Add files via upload

---
+++ code_challenge11.py
@@ -0,0 +1,8 @@
+for i in range(1,11,1):
+	for x in range(10, i, -1):
+		print(" ", end= " ")
+	for m in range(1, i, 1):
+		print("*", end= " ")
+	for l in range(1, i, 1):
+		print("*", end= " ")
+	print()
